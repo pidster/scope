0d64a225d7410dbbde5b56427583670d5d15ddfa
Alfonso Acosta <fons@weave.works>
2016-04-06 13:16:39 +0000

    Fix bug accessing wrong data
    However ... now I cannot access the data
    
    ...
    51: (63) *(u32 *)(r10 -60) = r7
    52: (79) r3 = *(u64 *)(r8 +216)
    R8 invalid mem access 'inv'

--- examples/plugins/ebpf/http-parse-simple-tracepoint.c
+++ examples/plugins/ebpf/http-parse-simple-tracepoint.c
@@ -62,12 +62,12 @@ int perf_trace_skb_copy_datagram_iovec_probe0(struct pt_regs *ctx )
   }
 
   u8 data[4] = {0, 0, 0, 0};
-  bpf_probe_read(&data, sizeof(data), &skb->data);
+  bpf_probe_read(&data, sizeof(data), skb->data);
   /* find a match with an HTTP message */
   bpf_trace_printk("Data1 %x %x\n", data[0], data[1]);
   bpf_trace_printk("Data2 %x %x\n", data[2], data[3]);
 
-  if ((data[0] != 'H') || (data[1] != 'T') || (data[2] != 'T') ||  (data[3] != 'P')) {
+  if ((data[0] != 'G') || (data[1] != 'E') || (data[2] != 'T')) {
     return 0;
   }
 
