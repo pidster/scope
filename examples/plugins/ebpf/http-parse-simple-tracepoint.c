2b899a2362b53bb99277d05c45e465c31e893e80
Alfonso Acosta <fons@weave.works>
2016-04-07 22:53:48 +0000

    Use a kprobe since the tracepoint doesn't provide an offset :(

--- examples/plugins/ebpf/http-parse-simple-tracepoint.c
+++ examples/plugins/ebpf/http-parse-simple-tracepoint.c
@@ -1,87 +1,62 @@
-/* generated from sudo /usr/share/bcc/tools/argdist -v -C 't:skb:skb_copy_datagram_iovec():int:tp.len' -i 2 */
-struct __string_t { char s[80]; };
-
 #include <uapi/linux/ptrace.h>
 #include <linux/skbuff.h>
 
-
-BPF_HASH(__trace_di, u64, u64);
-
-int __trace_entry_update(struct pt_regs *ctx)
-{
-  u64 tid = bpf_get_current_pid_tgid();
-  u64 val = ctx->di;
-  __trace_di.update(&tid, &val);
-  return 0;
-}
-
-struct skb_copy_datagram_iovec_trace_entry {
-  u64 __do_not_use__;
-  const void * skbaddr;
-  int len;
-
-};
-
-struct perf_trace_skb_copy_datagram_iovec_hash0_key_t {
+struct received_http_requests_key_t {
   u32 pid;
 };
-BPF_HASH(perf_trace_skb_copy_datagram_iovec_hash0, struct perf_trace_skb_copy_datagram_iovec_hash0_key_t, u64);
+BPF_HASH(received_http_requests, struct received_http_requests_key_t, u64);
 
 
-/* TODO: Can't we optimize this and do all the work directly in __trace_entry_update? */
-int perf_trace_skb_copy_datagram_iovec_probe0(struct pt_regs *ctx )
+/*
+  skb_copy_datagram_iter() (Kernels >= 3.19) is in charge of copying socket
+  buffers from kernel to userspace.
+
+  skb_copy_datagram_iter() has an associated tracepoint
+  (trace_skb_copy_datagram_iovec), which would be more stable than a kprobe but
+  it lacks the offset argument.
+ */
+int kprobe__skb_copy_datagram_iter(struct pt_regs *ctx, const struct sk_buff *skb, int offset, void *unused_iovec, int len)
 {
 
+  /* Inspect the beginning of docket buffers copied to user-space to determine if they
+     correspond to http requests.
 
-  u64 tid = bpf_get_current_pid_tgid();
-  u64 *di = __trace_di.lookup(&tid);
-  if (di == 0) { return 0; }
-  struct skb_copy_datagram_iovec_trace_entry tp = {};
-  bpf_probe_read(&tp, sizeof(tp), (void *)*di);
+     Caveats:
+
+     Requests may not appear at the beginning of a packet due to:
+     * Persistent connections.
+     * Packet fragmentation.
+
+     We could inspect the full packet but:
+     * It's very inefficient.
+     * Examining the non-linear (paginated) area of a socket buffer would be
+     really tricky from ebpf.
+   */
+
   /* TODO: exit early if it's not TCP */
-  const struct sk_buff *skb = tp.skbaddr;
-  /* TODO: offset is missing from tp,
-     this may result in incorrect request metrics
 
-     http://stackoverflow.com/questions/25047905/http-request-minimum-size-in-bytes
+  /* Inline implementation of skb_headlen() */
+  unsigned int head_len = skb->len - skb->data_len;
+
+  /* http://stackoverflow.com/questions/25047905/http-request-minimum-size-in-bytes
      minimum length of http request is always geater than 7 bytes
-     avoid invalid access memory
-     include empty payload
   */
-
-  /* Explicit implementation of skb_headlen() */
-  unsigned int skb_len = 0;
-  unsigned int skb_data_len = 0;
-  bpf_probe_read(&skb_len, sizeof(skb_len), &skb->len);
-  bpf_probe_read(&skb_data_len, sizeof(skb_data_len), &skb->data_len);
-  unsigned int head_len = skb_len - skb_data_len;
-  /* Print debug info (made available at /sys/kernel/debug/tracing/trace) */
-  bpf_trace_printk("Head_len  %u\n", head_len);
-  if (head_len < 7) {
+  if (head_len - offset < 7) {
     return 0;
   }
 
-  u8 data[4] = {0, 0, 0, 0};
-  void *datap = 0;
-  bpf_probe_read(&datap, sizeof(datap), &skb->data);
-  bpf_probe_read(&data, sizeof(data), datap);
-  /* find a match with an HTTP message */
-  bpf_trace_printk("Data1 %x %x\n", data[0], data[1]);
-  bpf_trace_printk("Data2 %x %x\n", data[2], data[3]);
+  u8 data[4] = {};
+  bpf_probe_read(&data, sizeof(data), skb->data + offset);
 
-  if ((data[0] != 'G') || (data[1] != 'E') || (data[2] != 'T')) {
+  /* TODO: support other methods and optimize lookups */
+  if ((data[0] == 'G') || (data[1] == 'E') || (data[2] == 'T')) {
     return 0;
   }
 
   /* Record request */
-  struct perf_trace_skb_copy_datagram_iovec_hash0_key_t __key = {};
-  __key.pid = tid & 0xFFFF;
+  struct received_http_requests_key_t key = {};
+  key.pid = bpf_get_current_pid_tgid();
+  received_http_requests.increment(key);
 
-  perf_trace_skb_copy_datagram_iovec_hash0.increment(__key);
   return 0;
 }
-
-/*
-open uprobes: {}
-open kprobes: {'p_tracing_generic_entry_update': c_void_p(33919360), 'r_perf_trace_skb_copy_datagram_iovec': c_void_p(33946304)}
-*/
--- examples/plugins/ebpf/http-parse-simple-tracepoint.py
+++ examples/plugins/ebpf/http-parse-simple-tracepoint.py
@@ -1,26 +1,15 @@
-# stripped-down version of: sudo /usr/share/bcc/tools/argdist -v -C 't:skb:skb_copy_datagram_iovec():int:tp.len' -i 2
-
-from bcc import BPF, Tracepoint
+from bcc import BPF
 from time import sleep, strftime
 
-b = BPF(src_file = "http-parse-simple-tracepoint.c")
-
-tp_category = "skb"
-tp_event = "skb_copy_datagram_iovec"
-probe_table_name = "perf_trace_skb_copy_datagram_iovec_hash0"
-probe_fn_name = "perf_trace_skb_copy_datagram_iovec_probe0"
-
-Tracepoint.enable_tracepoint(tp_category, tp_event)
-b.attach_kretprobe(event="perf_trace_" + tp_event, fn_name=probe_fn_name)
-Tracepoint.attach(b)
-
+b = BPF(src_file="http-parse-simple-tracepoint.c")
+print BPF.open_kprobes()
 while True:
     try:
         sleep(2)
     except KeyboardInterrupt:
         exit()
     print "[%s]" % strftime("%H:%M:%S")
-    data = b.get_table(probe_table_name)
+    data = b.get_table("received_http_requests")
     data = sorted(data.items(), key=lambda kv: kv[1].value)
     for key, value in data:
         print "\t%-10s %s" % (key.pid ,str(value.value))
